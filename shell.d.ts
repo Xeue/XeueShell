d96a0207e4103f7a3bc8974c1e7ff8192727f0ac
Sam Chilton <sam@xeue.uk>
2024-10-13 22:46:04 +0100

    Fixed type definitions

--- shell.d.ts
+++ shell.d.ts
@@ -1,19 +1,20 @@
 declare module 'xeue-shell';
+import type { ChildProcess } from "child_process"
 
 export class Shell {
     constructor(
         logger: Object,
         logsText: string,
         logsLevel: string,
-        shell: string
-    ) {}
+        shell?: string
+    );
 
 	run(
 		command: string,
 		doPrint: boolean
 	): Promise<{
-		stdout: arr[],
-		stderr: arr[],
+		stdout: Array<string>,
+		stderr: Array<string>,
 		hasErrors: boolean,
 		execProcess: ChildProcess
 	}>
@@ -27,7 +28,8 @@ export class Shell {
 		doPrint: boolean
 	): {
 		kill: Function,
-		execProcess: ChildProcess
+		execProcess: ChildProcess,
+		on: Function
 	}
 
 	bash(
@@ -38,3 +40,5 @@ export class Shell {
 		run: Function
 	}
 }
+
+export type {ChildProcess} from "child_process"
